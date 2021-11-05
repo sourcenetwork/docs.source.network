fddf7dfaec4aa3571351ee8f499f3d8255261859
Ashutosh Pradhan <ashutoshpradhan82@gmail.com>
2021-11-05 19:59:52 -0400

    updated linking

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'getting-started',
             position: 'left',
             label: 'Getting Started',
           },
