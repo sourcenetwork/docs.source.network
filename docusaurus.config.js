c535d893be3b4ef946d4f96f3991f514273a3efa
Orpheus Lummis <o@orpheuslummis.info>
2023-04-09 19:35:51 -0400

    remove blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -107,10 +107,6 @@ const config = {
             title: "More",
             items: [
               {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
                 href: "https://github.com/sourcenetwork",
               },
