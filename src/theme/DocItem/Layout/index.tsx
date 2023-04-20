f81bb47bef88b7b92f30fa2f9f2644a864233f53
David Turner <daveturner@live.com>
2023-04-20 18:45:36 -0400

    fix: theme issues (#91)

--- src/theme/DocItem/Layout/index.tsx
+++ src/theme/DocItem/Layout/index.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import clsx from "clsx";
-import { useWindowSize } from "@docusaurus/theme-common";
-import { useDoc } from "@docusaurus/theme-common/internal";
-import DocItemPaginator from "@theme/DocItem/Paginator";
-import DocVersionBanner from "@theme/DocVersionBanner";
-import DocVersionBadge from "@theme/DocVersionBadge";
-import DocItemFooter from "@theme/DocItem/Footer";
-import DocItemTOCMobile from "@theme/DocItem/TOC/Mobile";
-import DocItemTOCDesktop from "@theme/DocItem/TOC/Desktop";
-import DocItemContent from "@theme/DocItem/Content";
+import { useThemeConfig, useWindowSize } from "@docusaurus/theme-common";
+import { useDoc, useFilteredAndTreeifiedTOC } from "@docusaurus/theme-common/internal";
 import DocBreadcrumbs from "@theme/DocBreadcrumbs";
-import styles from "./styles.module.scss";
+import DocItemContent from "@theme/DocItem/Content";
+import DocItemFooter from "@theme/DocItem/Footer";
+import DocItemPaginator from "@theme/DocItem/Paginator";
+import DocItemTOCDesktop from "@theme/DocItem/TOC/Desktop";
+import DocItemTOCMobile from "@theme/DocItem/TOC/Mobile";
+import DocVersionBadge from "@theme/DocVersionBadge";
+import DocVersionBanner from "@theme/DocVersionBanner";
 import EditThisPage from "@theme/EditThisPage";
+import clsx from "clsx";
+import React from "react";
+import styles from "./styles.module.scss";
 
 /**
  * Decide if the toc should be rendered, on mobile or desktop viewports
@@ -19,8 +19,17 @@ import EditThisPage from "@theme/EditThisPage";
 function useDocTOC() {
   const { frontMatter, toc } = useDoc();
   const windowSize = useWindowSize();
+  const themeConfig = useThemeConfig();
+
+  // Get the rendered TOC tree to determine visibility
+  const tocTree = useFilteredAndTreeifiedTOC({
+    toc,
+    minHeadingLevel: frontMatter.toc_min_heading_level ?? themeConfig.tableOfContents.minHeadingLevel,
+    maxHeadingLevel: frontMatter.toc_max_heading_level ?? themeConfig.tableOfContents.maxHeadingLevel,
+  });
+
   const hidden = frontMatter.hide_table_of_contents;
-  const canRender = !hidden && toc.length > 0;
+  const canRender = !hidden && tocTree.length > 0;
   const mobile = canRender ? <DocItemTOCMobile /> : undefined;
   const desktop =
     canRender && (windowSize === "desktop" || windowSize === "ssr") ? (
@@ -53,7 +62,7 @@ export default function DocItemLayout({ children }) {
         </div>
 
         <div className={clsx('col col--3', styles.sidebar)}>
-          {docTOC.desktop && <>
+          {docTOC.desktop &&
             <div className={styles.sidebarItems}>
               <div>{metadata.title}</div>
               {docTOC.desktop}
@@ -63,7 +72,7 @@ export default function DocItemLayout({ children }) {
                 </div>
               )}
             </div>
-          </>}
+          }
         </div>
       </div>
     </div>
