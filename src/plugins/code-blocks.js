fd8455158bdfd83667380b05fe5fc77e8fbc9528
ribiza <stefano@source.network>
2026-06-18 09:51:05 +0200

    polish collapsible code blocks implementation: - support for hidden blocks (as in tabbed examples) - better support for hot reloading

--- src/plugins/code-blocks.js
+++ src/plugins/code-blocks.js
@@ -1,65 +1,76 @@
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 var maxLines = 8  // css 150px is in sync with this
+var lineTolerance = 4  // don't collapse if block is longer but within tolerance
+var collapsedClassName = 'collapsed'
 
-const collapseBlocks = () => {
+function collapseBlocks() {
   let blocks = document.querySelectorAll('main div.theme-code-block')
-  if ( blocks.length == 0 ) return
-  let randomBlockStyle = window.getComputedStyle(blocks[0].querySelector('pre'))
-  // line-height px value for pre inside code blocks
-  let lineHeight = parseFloat(randomBlockStyle.getPropertyValue('line-height'))
+
+  if ( blocks.length == 0 || isPageAlreadyCollapsed(blocks) ) return
 
   blocks.forEach(collapseBlock)
 
-  function collapseBlock(block) {
-      if ( ! shouldCollapseBlock(block) ) return
-      let pre = block.querySelector('pre')
-      let totalLines = Math.ceil(pre.clientHeight / lineHeight)  // must compute before collapsing
-      pre.classList.add('collapsed')
+function collapseBlock(block) {
+  if ( ! shouldCollapseBlock(block) ) return
 
-      let displayAll = document.createElement('div')
-      displayAll.classList.add('display-all')
-      pre.addEventListener('click', expandBlock)  // clicking anywhere on the code block expands it
-      let displayAllLink = document.createElement('a')
-      displayAllLink.innerHTML = `Expand (${totalLines} lines)`
-      block.appendChild(displayAll)
-      displayAll.appendChild(displayAllLink)
-  }
+  // Collapse
+  let pre = block.querySelector('pre')
+  pre.classList.add(collapsedClassName)
 
-  function shouldCollapseBlock(block) {
-    let lineTolerance = 4 // don't collapse if block is longer but within tolerance
-    if ( block.offsetHeight < (maxLines + lineTolerance) * lineHeight )
-      return false
-    if ( block.querySelector('.code-block-collapse') != null )
-      return true
-    if ( block.querySelector('.code-block-no-collapse') != null )
-      return false
-    if ( block.querySelector('.codeBlockTitle_OeMC') != null
-    && block.querySelector('.codeBlockTitle_OeMC').textContent == 'Result' )
-      return true
-    return false
-  }
-
-  function expandBlock(e) {
-    e.preventDefault()
-    let block = e.target.closest('div.theme-code-block')
-    let pre = block.querySelector('pre')
-    pre.classList.remove('collapsed')
-    block.querySelector('div.display-all')?.remove()
-  }
+  // Link to expand
+  let displayAll = document.createElement('div')
+  displayAll.classList.add('display-all')
+  pre.addEventListener('click', expandBlock)  // clicking anywhere on the code block expands it
+  let displayAllLink = document.createElement('a')
+  displayAllLink.innerHTML = `Expand (${blockLineCount(block)} lines)`
+  block.appendChild(displayAll)
+  displayAll.appendChild(displayAllLink)
 }
 
+function shouldCollapseBlock(block) {
+  if ( blockLineCount(block) < maxLines + lineTolerance )
+    return false
+  if ( block.querySelector('.code-block-collapse') != null )
+    return true
+  if ( block.querySelector('.code-block-no-collapse') != null )
+    return false
+  if ( block.querySelector('.codeBlockTitle_OeMC') != null
+  && block.querySelector('.codeBlockTitle_OeMC').textContent == 'Result' )
+    return true
+  return false
+}
+
+function expandBlock(e) {
+  e.preventDefault()
+  let block = e.target.closest('div.theme-code-block')  // click can come from anywhere inside the block
+  let pre = block.querySelector('pre')
+  pre.classList.remove(collapsedClassName)
+  block.querySelector('div.display-all')?.remove()
+}
+
+function blockLineCount(block) {
+  let pre = block.querySelector('pre')
+  let lineCount = pre.innerHTML.split('<div class="token-line"').length - 1
+  return lineCount
+}
+
+function isPageAlreadyCollapsed(blocks) {
+  // If at least one block has the collapsed class, we've already been here.
+  // This is to avoid re-executing when navigating between headings.
+  return Array.from(blocks).some( (b) =>
+    b.querySelector('pre').classList.contains(collapsedClassName)
+  )
+}
+}
+
+// For react re-paints, after first page load
 export function onRouteDidUpdate({location, previousLocation}) {
-  // Don't execute if we are still on the same page; the lifecycle may be fired
-  // because the hash changes (e.g. when navigating between headings)
-  if (location.pathname === previousLocation?.pathname) return;
   collapseBlocks();
 }
 
+// For page loads
 if (ExecutionEnvironment.canUseDOM) {
-  // We also need to setCodeRevealTriggers when the page first loads; otherwise,
-  // after reloading the page, these triggers will not be set until the user
-  // navigates somewhere.
   window.addEventListener('load', () => {
     setTimeout(collapseBlocks, 1000);
   });
