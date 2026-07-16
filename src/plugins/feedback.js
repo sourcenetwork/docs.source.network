9c654c508affcfef291df32127cd9629b4f0cfb7
ribiza <stefano@source.network>
2026-07-16 07:03:43 +0200

    self review

--- src/plugins/feedback.js
+++ src/plugins/feedback.js
@@ -6,9 +6,7 @@ function removeFooterThumbs() {
   let feedbackWidgetDiv = feedbackThumbsUp.closest('.feedback-widget')
   
   // remove thumbs elements
-  feedbackWidgetDiv
-    .querySelectorAll('span')
-    .forEach((el) => {el.remove()})
+  feedbackWidgetDiv.querySelectorAll('span').forEach((el) => {el.remove()})
   
   // acknowledge their kindness (once)
   if (feedbackWidgetDiv.querySelector('div.feedback-thanks') != null) return;
@@ -19,28 +17,25 @@ function removeFooterThumbs() {
 }
 
 function registerFeedbackHandlers() {
-  // The Yes button submits feedback immediately, while the No button sends to the modal
+  // The Yes button submits feedback immediately, while the No button sends to the modal,
+  // so we don't need to remove thumbs on negative feedback.
   document.querySelector('.feedback-widget button[title="Yes"]').addEventListener('click', (event) => {
     removeFooterThumbs();
   })
   
+  // When feedback is sent (through the modal), remove footer widget
   document.addEventListener('feedbackSent', (event) => {
     const feedback = event.detail.feedback;
     console.log('Feedback submitted:', feedback);
     removeFooterThumbs();
   });
   
+  // Any error on submission should still allow to submit again
   document.addEventListener('feedbackError', (event) => {
     const error = event.detail.error;
     console.error('Feedback submission error:', error);
-    removeFooterThumbs();
   });
 }
-
-// For react re-paints, after first page load
-//export function onRouteDidUpdate({location, previousLocation}) {
-//  registerFeedbackHandlers();
-//}
 
 // For page loads
 if (ExecutionEnvironment.canUseDOM && ExecutionEnvironment.canUseEventListeners) {
