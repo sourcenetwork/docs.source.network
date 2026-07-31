05e64178503f42a8615196a755316ccdbd2fda28
Anuraag (Rag) Agrawal <anuraaga@gmail.com>
2026-07-31 15:07:00 +0900

    Fix link to About Us

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -150,7 +150,7 @@ const config = {
             items: [
               {
                 label: "About Us",
-                href: "https://source.network/about",
+                href: "https://source.network/our-story",
               },
               {
                 label: "Privacy Policy",
